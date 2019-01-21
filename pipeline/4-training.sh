2e844c30ddfdd45b9697fbd47a5a057561c25e26
Yuan Chuan Kee <yuanchuan@u.nus.edu>
2019-01-21 10:39:11 +0800

    Fixed wrong logic for CV

--- pipeline/4-training.sh
+++ pipeline/4-training.sh
@@ -23,19 +23,9 @@ do
     --lower $LOWER \
     --cap_dim $CAP_DIM \
     --zeros $ZEROES \
-    --reload 1 > $PATH_TO_RESULTS/ETD_${WORD_DIM}_${LOWER}_${CAP_DIM}_${ZEROES}_${DATA_ID}_fold_$d.out
-  else
-    echo "Running first fold" && python train.py \
-    --train $PATH_TO_FOLDS/$d/train.txt \
-    --test $PATH_TO_TEST \
-    --dev $PATH_TO_FOLDS/$d/val.txt \
-    --pre_emb $PATH_TO_WB \
-    --word_dim $WORD_DIM \
-    --lower $LOWER \
-    --cap_dim $CAP_DIM \
-    --zeros $ZEROES > $PATH_TO_RESULTS/ETD_${WORD_DIM}_${LOWER}_${CAP_DIM}_${ZEROES}_${DATA_ID}_fold_$d.out
+    --reload 0 > $PATH_TO_RESULTS/ETD_${WORD_DIM}_${LOWER}_${CAP_DIM}_${ZEROES}_${DATA_ID}_fold_$d.out
   fi
-
+  
   if [[ $? -eq 0 ]]; then
     echo "$d fold has completed successfully."
   fi
