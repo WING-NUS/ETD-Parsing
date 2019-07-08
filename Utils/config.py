4afe472fa04913990ed0608b59132d883c590492
Jian Wu <fanchyna@gmail.com>
2019-07-07 21:12:28 -0400

    Create config.py
    a template of config.py

---
+++ Utils/config.py
@@ -0,0 +1,11 @@
+# configuration for db2bibtex.py
+# top level directory to save output and other related data
+workdir="/data/workdir"
+acmdldb = {
+    'user': 'user',
+    'password': 'password',
+    'host': 'localhost',
+    'database': 'acmdata2018',
+    'unix_socket': '/var/run/mysqld/mysqld.sock',
+    'raise_on_warnings': True
+}
